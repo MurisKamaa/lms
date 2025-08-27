71bb04c63a89553146b0e5dc73bb021f0515d79c
MurisKamaa <engpaulkamau@gmail.com>
2025-08-27 17:49:33 +0300

    Created an exceptions file to handle not available and not found

---
+++ exceptions/custom_exceptions.py
@@ -0,0 +1,7 @@
+class BookNotAvailableError(Exception):
+    def __init__(self, book_title: str) -> None:
+        super().__init__(f"The book {book_title} is currently not available")
+
+class BookNotFoundError(Exception):
+    def __init__(self, book_title: str) -> None:
+        super().__init__(f"The book {book_title} cannot be found")
