6fb3af01a3bfe354c1ef36a6e43b94f30625f101
MurisKamaa <engpaulkamau@gmail.com>
2025-08-27 17:51:30 +0300

    Created the Library object to handle what the library does

---
+++ models/library.py
@@ -0,0 +1,21 @@
+from models.users import User
+from models.book import Book
+
+
+class Library:
+    def __init__(self, name, ) -> None:
+        self.name = name
+        self.books: list[Book] = []
+        self.users: User = []
+
+    def add_book(self, book: Book) -> None:
+        self.books.append(book)
+
+    def register_user(self, user: User) -> None:
+        self.users.append(user)
+
+    def find_book(self, title: str) -> Book | None:
+        for book in self.books:
+            if title == book.title:
+                return book
+        return None
